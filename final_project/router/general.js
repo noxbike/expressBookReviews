2e5cc88c655debb26352765813ee36aa12383edb
noxbike <noxbike@gmail.com>
2024-09-12 07:04:18 -0400

    Task 3: get books by author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -25,9 +25,9 @@ public_users.get('/',function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  const { isbn } = req.params
+  const { isbn } = req.params;
 
-  if (books[isbn] ){
+  if (books[isbn]) {
     return res.status(300).json({message: books[isbn]})
   } else {
     return res.status(404).json({message: "book not found!"})
@@ -37,7 +37,20 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const { author } = req.params;
+  const list = [];
+
+  for (book in books) {
+      if (books[book]['author'] == author) {
+        list.push(books[book])
+    }
+  }
+  
+  if (list.length > 0) {
+    return res.status(300).json({message: list});
+  } else {
+    return res.status(404).json({message: "author not found"})
+  }
 });
 
 // Get all books based on title
