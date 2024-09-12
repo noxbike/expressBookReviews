ea8f65d84f3798e43770ee1282bd35e17b2c7893
noxbike <noxbike@gmail.com>
2024-09-12 08:29:46 -0400

    Task 5: get review of book by isbn

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -69,7 +69,13 @@ public_users.get('/title/:title',function (req, res) {
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const { isbn } = req.params;
+
+    if (books[isbn]) {
+        return res.status(300).json({message: books[isbn]['reviews']})
+    } else {
+        return res.status(404).json({message: "book not found!"});
+    }
 });
 
 module.exports.general = public_users;
